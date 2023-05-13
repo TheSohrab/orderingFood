44fd781844d3d12e5d1f7ea4dea041d1f3fb8254
TheSohrab <131244568+TheSohrab@users.noreply.github.com>
2023-05-13 17:20:01 +0330

    Add files via upload

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>“Welcome to Sohrab’s first site”</h2>
+      <p>created using CSS modules, JavaScript, and React.</p>
+    </section>
+  );
+};
+
+export default MealsSummary;
